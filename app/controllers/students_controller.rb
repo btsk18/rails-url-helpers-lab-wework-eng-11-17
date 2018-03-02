1abf32112f2bfe70f26ab2804fc5a6059eebefe1
Brandon Leeds <brandonleeds@gmail.com>
2018-03-02 18:17:58 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,12 +2,13 @@ require 'pry'
 
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    binding.pry
   end
 
   private
