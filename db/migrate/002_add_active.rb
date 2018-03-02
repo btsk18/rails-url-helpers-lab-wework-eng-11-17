88344717e08afd50eb4c8aefd5bb63ab37654153
Brandon Leeds <brandonleeds@gmail.com>
2018-03-02 19:04:57 +0000

    automatically backed up by learn

--- db/migrate/002_add_active.rb
+++ db/migrate/002_add_active.rb
@@ -1,5 +1,5 @@
 class AddActive < ActiveRecord::Migration
   def change
-    add_column :students, :active, :boolean, default: true
+    add_column :students, :active, :boolean, :default => 1
   end
 end
